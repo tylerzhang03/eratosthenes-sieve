6168ef1814498c1145c77fbdac94db1ea4879bf0
Tyler Zhang <ty.zhang03@gmail.com>
2022-12-01 11:32:03 -0500

    Add files via upload

---
+++ eratosthenes-sieve/Main.java
@@ -0,0 +1,18 @@
+package data_structures_hw3;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sieve newSieve = new Sieve();
+		Scanner scan = new Scanner(System.in);
+		int n;
+		
+		System.out.println("Please enter upper bound");
+		n = Integer.valueOf(scan.nextLine());
+		
+		newSieve.primesTo(n);
+	}
+
+}
---
+++ eratosthenes-sieve/Sieve.java
@@ -0,0 +1,50 @@
+package data_structures_hw3;
+
+public class Sieve {
+	
+	private ArrayQueue<Integer> numbers;
+	private ArrayQueue<Integer> primes;
+	private ArrayQueue<Integer> newNums;
+	
+	public Sieve() {
+		numbers = new ArrayQueue<>();
+		primes = new ArrayQueue<>();
+		newNums = new ArrayQueue<>();
+	}
+	
+	public void primesTo(int n) {
+		if (n < 2) throw new IllegalArgumentException("Error: Input must be a number greater than 2.");
+		
+		for (int i = 2; i <= n; i++) {
+			numbers.enqueue(i);
+		}
+		
+		int p = numbers.first();
+		while (p <= Math.sqrt(n)) {
+			primes.enqueue(p);
+			eliminate(p);
+			p = numbers.first();
+		}
+		
+		while (!numbers.isEmpty()) {
+			primes.enqueue(numbers.dequeue());
+		}
+		
+		System.out.print("Primes up to " + n + " are: ");
+		System.out.print(primes.dequeue());
+		while (!primes.isEmpty()) {
+			System.out.print(", " + primes.dequeue());
+		}
+	}
+	
+	private void eliminate(int p) {
+		while (!numbers.isEmpty()) {
+			if (numbers.first() % p == 0) {
+				numbers.dequeue();
+			} else newNums.enqueue(numbers.dequeue());
+		}
+		while (!newNums.isEmpty()) {
+			numbers.enqueue(newNums.dequeue());
+		}
+	}
+}
